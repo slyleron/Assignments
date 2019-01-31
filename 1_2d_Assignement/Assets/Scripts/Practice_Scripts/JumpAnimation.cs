056d5bd51bbb38b9060fbc610f92ec99942bddec
skyler <skyleryoung1111@gmail.com>
2019-01-31 11:03:40 -0700

    AnimationScripts
    Added script for jump, also improved artwork

--- 1_2d_Assignement/Assets/Scripts/Practice_Scripts/JumpAnimation.cs
+++ 1_2d_Assignement/Assets/Scripts/Practice_Scripts/JumpAnimation.cs
@@ -3,30 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JumpAnimation : MonoBehaviour {
-    public Sprite Reachingup2;
-    public Sprite Reachingup1;
+    public Sprite reachingup2;
+    public Sprite reachingup3;
+    public Sprite running;
+    public float waittime=.25f;
+    public string updown = "up";
     // Use this for initialization
     void Start () {
-		
-	}
+        waittime = .25f;
+        updown = "up";
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown  (KeyCode.Space))
         {
-            SpriteChangerJump();
+            if (updown == "up")
+            {
+                StartCoroutine(SpriteChangerJump(waittime));
+            }
+            else
+            {
+                StartCoroutine(SpriteChangerFall(waittime));
+            }
+        }
+        
 
-        }
-	}
-    void SpriteChangerJump()
-    {
-        if (SpriteRenderer.Sprite == Reachingup2) 
-        {
-            SpriteRenderer.Sprite = Reachingup1;
-        }
-        else
-        {
-            SpriteRenderer.Sprite = Reachingup2; 
-        }
+
     }
+    IEnumerator SpriteChangerJump(float waittime)
+    {
+        
+        this.GetComponent<SpriteRenderer>().sprite = reachingup2;
+        yield return new WaitForSeconds(waittime);
+        print("Success");
+        this.GetComponent<SpriteRenderer>().sprite = reachingup3;
+        updown = "down";
+
+    }
+    IEnumerator SpriteChangerFall(float waittime)
+    {
+
+        this.GetComponent<SpriteRenderer>().sprite = reachingup2;
+        yield return new WaitForSeconds(waittime);
+        print("Success");
+        this.GetComponent<SpriteRenderer>().sprite = running;
+        updown = "up";
+
+    }
+
 }
