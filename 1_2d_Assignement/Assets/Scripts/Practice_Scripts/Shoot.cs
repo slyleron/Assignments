af32dbb48e7f5784e40b18a82d9b52c09210e79c
Skyler <skyleryoung1111@gmail.com>
2019-02-25 17:04:57 -0700

    Scalingcoding

--- 1_2d_Assignement/Assets/Scripts/Practice_Scripts/Shoot.cs
+++ 1_2d_Assignement/Assets/Scripts/Practice_Scripts/Shoot.cs
@@ -4,39 +4,32 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour {
     private bool contactToStop;
-    private float size;
+    public float size;
     private GameObject empty;
     Rigidbody2D m_Rigidbody;
     private bool canpickup;
+    public GameObject goo;
 
     // Use this for initialization
     void Start () {
         m_Rigidbody = GetComponent<Rigidbody2D>();
-
+        size = .25f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-       
-		
-	}
+        if (goo.GetComponent<SpriteRenderer>().transform.localScale.x > .25)
+        {
+            size = 0.01f;
+        }
+        
+
+
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            if (canpickup == true)
-            {
-                Destroy(gameObject);
-            }
-            else
-            {
-                m_Rigidbody.constraints = RigidbodyConstraints2D.None;
-                Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
-                print("step one");
-                
-            }
-        }
-        else if (collision.gameObject.tag == "ground")
+        if (collision.gameObject.tag != "Player")
             {
                 gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
                 m_Rigidbody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
@@ -48,4 +41,18 @@ public class Shoot : MonoBehaviour {
         
 
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player"&canpickup==true)
+        {
+            Destroy(gameObject);
+            goo.GetComponent<SpriteRenderer>().transform.localScale = new Vector2(goo.GetComponent<SpriteRenderer>().transform.localScale.x+size, goo.GetComponent<SpriteRenderer>().transform.localScale.y+size);
+        }
+        if (other.tag == "Player" & canpickup == false)
+        {
+            
+            goo.GetComponent<SpriteRenderer>().transform.localScale = new Vector2(goo.GetComponent<SpriteRenderer>().transform.localScale.x - size, goo.GetComponent<SpriteRenderer>().transform.localScale.y - size);
+        }
+    }
+    
 }
