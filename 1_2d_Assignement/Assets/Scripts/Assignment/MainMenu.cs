2047df33bb114980f9163dc52c1da8f4447ab532
Skyler <skyleryoung1111@gmail.com>
2019-03-13 17:45:55 -0600

    MainMenu and Jumper
    Made the main menu screen and the jumper

---
+++ 1_2d_Assignement/Assets/Scripts/Assignment/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+   
+    //Which level to load
+    public int levelToLoad;
+    //function to call loading level
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+    public void RunForestRun()
+    {
+        SceneManager.LoadScene("RunForestRun");
+    }
+    public void Tank()
+    {
+        SceneManager.LoadScene("Tank");
+    }
+    public void Painter()
+    {
+        SceneManager.LoadScene("Painter");
+    }
+    public void Skiier()
+    {
+        SceneManager.LoadScene("Skiier");
+    }
+    public void ArrowShooter()
+    {
+        SceneManager.LoadScene("ArrowShooter");
+    }
+    
+
+
+    //exit will not work at this time due to it not being an actual game
+    public void LevelExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ 1_2d_Assignement/Assets/Scripts/Practice_Scripts/RunJumpLand.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunJumpLand : MonoBehaviour {
+    private GameObject bodyMain;
+    private float jumphight;
+    // Use this for initialization
+    void Start () {
+		bodyMain= GameObject.FindGameObjectWithTag("Player");
+        jumphight = 7f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            bodyMain.GetComponent<Rigidbody2D>().velocity = new Vector2(bodyMain.GetComponent<Rigidbody2D>().velocity.x, bodyMain.GetComponent<Rigidbody2D>().velocity.y+jumphight);
+        }
+	}
+}
